32c45cdb49538e050aee98d15108140be438be6a
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-03-15 14:44:58 -0300

    Agora acende o LED

--- leitura_RFID/leitor_RFID.ino
+++ leitura_RFID/leitor_RFID.ino
@@ -1,5 +1,8 @@
 #include <SoftwareSerial.h> 
 
+const int ledPin = 7;
+
+
 // Esse é o Modulo de RFID nos pinos 2 e 3
 SoftwareSerial mySerial(2, 3);
 //Array que armazenará cada byte em HEX lido pelo RFID
@@ -8,7 +11,7 @@ int leitura[12];
 // Pega o id leitor
 char comando_LeituraTag[] = { 0xAA , 0x00, 0x03, 0x25, 0x26, 0x00, 0x00, 0xBB };
 int tagValida1[4] = {135, 80, 114, 203};
-int anterior[4] = {0, 0, 0, 0};
+int anterior[4] = {-1, -1, -1, -1};
 
 
 void setup()
@@ -17,12 +20,13 @@ void setup()
   Serial.begin(57600);
   Serial.println("Iniciando leitor de RFID"); // Mensagem de inicio
   mySerial.begin(9600); // Taxa de leitura do modulo RFID
-  
+  pinMode(ledPin, OUTPUT);
+ 
 }
 
 void loop() 
 { 
-  delay(500);
+  delay(250);
   
   // Contador
   int j = 0;
@@ -35,20 +39,22 @@ void loop()
   }
 
   if (verificaTagValida() == true) {
-    // Retira apenas os Bytes com o ID dos tag
-    if (estaPressionado() == false) {
-      for (j = 5; j < 9; j++) { 
-        int i = j - 5;
-        
-        anterior[i] = leitura[j];
-        Serial.print(leitura[j], HEX);
-        //Serial.print(" - ");
-        //Serial.println(leitura[j]);
-      }
+
+    // Mudança de estado para estar pressionado
+    if (ehAnterior() == false) {
+        digitalWrite(ledPin, 1);
+
+      Serial.println("PRESSIONADO");
+    } 
       
-      Serial.println("\nEstá em cima do leitor \n ------------------------------------------");
-    }
   } else {
+    // Verifica se ele despressionou a tag do rfid
+    // Mudança de estado para estar não pressionado
+    if (ehAnterior() == false) {
+      digitalWrite(ledPin, 0);
+
+      Serial.println("NAUM PRESSIONADO");
+    } 
     
     // Reseta o anterior
     anterior[0] = -1;
@@ -57,6 +63,17 @@ void loop()
     anterior[3] = -1;
     
   }
+
+  for (j = 5; j < 9; j++) { 
+        int i = j - 5;
+           
+        anterior[i] = leitura[j];
+        //Serial.print(leitura[j], HEX);
+        //Serial.println(leitura[j]);
+  } 
+   
+
+  //Serial.println("NAU PRESSIONADO");
     
 
   // Envia comando para leitura do tag1
@@ -85,17 +102,18 @@ bool verificaTagValida() {
 }
 
 
-bool estaPressionado() {
-  // Compara se existe alguma tag que estamos procurando
+bool ehAnterior() {
+// Verifica se o mesma tag está em cima do leitor
+
   for (int j = 5; j < 9; j++) {
     int i = j - 5;
     
-    if (leitura[j] == anterior[i]) {
-      return true;
+    if (leitura[j] != anterior[i]) {
+      return false;
     }
   }
 
-  return false;
+  return true;
 }
 
 

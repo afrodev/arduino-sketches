9d657050dea363995dd361cbcc7afccc31e55f44
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-03-17 14:57:40 -0300

    Agora rodando tudo bunitu

--- leitura_RFID/leitor_RFID.ino
+++ leitura_RFID/leitor_RFID.ino
@@ -1,115 +1,134 @@
 #include <SoftwareSerial.h> 
 
+SoftwareSerial mySerial(2, 3);
 const int ledPin = 7;
 
-// Esse é o Modulo de RFID nos pinos 2 e 3
-SoftwareSerial mySerial(2, 3);
-//Array que armazenará cada byte em HEX lido pelo RFID
-int leitura[12];
 
-// Pega o id leitor
-char comando_LeituraTag[] = { 0xAA , 0x00, 0x03, 0x25, 0x26, 0x00, 0x00, 0xBB };
-int tagValida1[4] = {135, 80, 114, 203};
-int anterior[4] = {-1, -1, -1, -1};
+//MF_READ - Código para leitura
+byte MF_GET_SNR[15] = {0XAA, 0X00, 0X0A, 0X20, 0X01, 0X01, 0X10, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0x3A, 0XBB};
+
+// PARA ESCREVER 0FFFFF NA TAG
+//byte MF_GET_SNR[31] = {0XAA, 0X00, 0X1A, 0X21, 0X01, 0X01, 0X10, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0X2B, 0XBB}; //MF_WRITE 16 F (A)
+
+// PARA ESCREVER 0FFFF1 NA TAG 
+//byte MF_GET_SNR[31] = {0XAA, 0X00, 0X1A, 0X21, 0X01, 0X01, 0X10, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0XFF, 0X11, 0X11, 0X2B, 0XBB}; //MF_WRITE 14F 11 11 (B)
+
+// PARA ESCREVER 000000 NA TAG
+//byte MF_GET_SNR[31] = {0XAA, 0X00, 0X1A, 0X21, 0X01, 0X01, 0X10, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X2B, 0XBB}; //MF_WRITE 16 zeros (C)
+
+// PARA ESCREVER 0000001 NA TAG
+//byte MF_GET_SNR[31] = {0XAA, 0X00, 0X1A, 0X21, 0X01, 0X01, 0X10, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0Xff, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X11, 0X11, 0X2B, 0XBB}; //MF_WRITE 14 zeros 11 11 (D)
+
+
+// Texto que será escrito o rfid_leitura
+String rfid_leitura;
+
+// Grava a leitura anterior para comparar
+String rfid_anterior;
+
+
+// Verifica se está debug
+boolean debugging;
+
 
 void setup() {
-  // Taxa de leitura do terminal
+  // Inicializa todos os seriais
   Serial.begin(57600);
-  Serial.println("Iniciando leitor de RFID"); // Mensagem de inicio
-  mySerial.begin(9600); // Taxa de leitura do modulo RFID
+  
+  // Inicializa o modulo do rfid_leitura
+  mySerial.begin(9600);
 
-  // Fala que este pino acende o LED
+  // Debug 
+  debugging = true; 
+  Serial.println("SKETCH ABERTO");
+  
+  // Ativa o Botão 
   pinMode(ledPin, OUTPUT);
- 
+
+  rfid_anterior = "-1";
+
 }
 
-void loop() { 
-  delay(250);
-  
-  // Contador
-  int j = 0;
+void loop() {
+  // Escreve no modulo rfid, o comando de leitura
+  mySerial.write(MF_GET_SNR, sizeof(MF_GET_SNR));
+  delay(150); // Delay 
 
-  // Enquanto ele conseguir ler dados do modulo de RFID
-  while(mySerial.available()) {
-    // Ele lê e coloca no vetor de leitura
-    leitura[j] = mySerial.read();
-    j++;
-  }
+  // Inicializa o texto de rfid
+  rfid_leitura = "";
 
-  if (verificaTagValida() == true) {
-
-    // Mudança de estado para estar pressionado
-    if (ehAnterior() == false) {
-      digitalWrite(ledPin, 1);
-      imprimeAtual();
-      Serial.println("PRESSIONADO");
-    } 
+  // Enquanto o modulo estiver livre
+  while(mySerial.available() > 0) {
       
-  } else {
-    // Verifica se ele despressionou a tag do rfid
-    // Mudança de estado para estar não pressionado
-    if (ehAnterior() == false) {
-      digitalWrite(ledPin, 0);
+      // Leitura dos dados rfid
+      int x = mySerial.read();
+    
+      //Se é zero coloca na string 0
+      if(x == 0){
+        rfid_leitura += "0";
+      }
 
-      Serial.println("NAUM PRESSIONADO");
-    } 
-    
-    // Reseta o anterior
-    anterior[0] = -1;
-    anterior[1] = -1;
-    anterior[2] = -1;
-    anterior[3] = -1;
-    
+      // Leitura do segundo byte
+      x = mySerial.read();
+
+      //Se o byte lido for FF (255 em decimal), coloca um F na string
+      if(x == 255){
+        rfid_leitura += "F";
+      }
+
+      // Leitura do proximo byte
+      x = mySerial.read();
+      
+      // Se o byte for 0x11
+      if(x == 17) {
+        rfid_leitura += "1";
+      }  
   }
-  // Atualiza o anterior
-  for (j = 5; j < 9; j++) { 
-        int i = j - 5;
-        anterior[i] = leitura[j];
-  }     
 
-  // Envia comando para leitura do tag1
-  for (j = 0; j < 8; j++) 
-    mySerial.write(comando_LeituraTag[j]);
-
-  //Limpa o Buffer de leitura dos tags
-  for (j = 0; j < 12; j++)
-    leitura[j] = 0;
-    
-}
-
-// Verifica se a tag é valida   
-bool verificaTagValida() {
-  
-  // Compara se existe alguma tag que estamos procurando
-  for (int j = 5; j < 9; j++) {
-    int i = j - 5;
-    
-    if (leitura[j] != tagValida1[i]) {
-      return false;
-    }
+  // 
+  if (rfid_leitura == "0FFFFF"){         // 1
+      if (rfid_anterior != "0FFFFF") {
+        if(debugging)  {
+          rfid_anterior = rfid_leitura;
+          Serial.println("id01"); 
+          Serial.println(rfid_leitura);
+          digitalWrite(ledPin, 1);
+        }
+        else Serial.write(1);
+      }
+  } else if (rfid_leitura == "0FFFF1") { // 2
+      if (rfid_anterior != "0FFFF1") {
+        if(debugging){ 
+          rfid_anterior = rfid_leitura;          
+          Serial.println("id02"); 
+          Serial.println(rfid_leitura);
+          digitalWrite(ledPin, 1);
+        }
+        else Serial.write(2);
+      }
+  } else if (rfid_leitura == "000000") { // 3
+      if (rfid_anterior != "000000") {
+        if(debugging) {
+          rfid_anterior = rfid_leitura;
+          Serial.println("id03"); 
+          Serial.println(rfid_leitura); 
+          digitalWrite(ledPin, 1);
+        }
+        else Serial.write(3);
+      }
+  } else if(rfid_leitura == "0000001"){  // 4
+      if (rfid_anterior != "0000001") {
+        if(debugging){
+          rfid_anterior = rfid_leitura;
+          Serial.println("id04"); 
+          Serial.println(rfid_leitura);    
+          digitalWrite(ledPin, 1);
+        }
+        else Serial.write(4);
+      }
+  }else {
+        rfid_anterior = "-1";
+        digitalWrite(ledPin, 0);
   }
-  
-  return true;
+
 }
-
-
-bool ehAnterior() {
-  // Verifica se o mesma tag está em cima do leitor
-  for (int j = 5; j < 9; j++) {
-    int i = j - 5;
-    
-    if (leitura[j] != anterior[i]) {
-      return false; 
-    }
-  }
-  
-  return true;
-}
-
-void imprimeAtual() {
-  for (int j = 5; j < 9; j++) 
-    Serial.print(leitura[j], HEX);
-  Serial.println();
-}
-
-
